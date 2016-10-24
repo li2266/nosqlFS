8750fbdae0260166093770fc1008e4b672464958
li2266 <li2266@purdue.edu>
2016-10-24 15:22:58 -0400

    successfully complied and need more functions

--- src/log.c
+++ src/log.c
@@ -1,9 +1,17 @@
+#include "nosqlFS.h"
+
 #include <stdio.h>
 #include <errno.h>
 #include <fuse.h>
 #include <time.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
+#include "log.h"
+
+char time_buf[32];
 /*
 create a new log file or append log at the end of a exist file
 */
@@ -15,7 +23,7 @@ FILE * log_open(){
   struct tm *p = gmtime(&now);
   // create log file name
   char log_name[30];
-  sprinft(log_name, "nosqlFS_%d-%d-%d.log", p->tm_mon + 1, p->tm_mday, p->tm_year + 1900 );
+  sprintf(log_name, "nosqlFS_%d-%d-%d.log", p->tm_mon + 1, p->tm_mday, p->tm_year + 1900 );
   // open the file and check if successful
   logFile = fopen(log_name, "a");
   if(logFile == NULL){
@@ -31,7 +39,10 @@ basic log function. write a line into logfile.
 void log_msg(const char *format, ...){
   va_list ap;
   va_start(ap, format);
-  vfprint(nosqlFS_Data->logFile, format, ap);
+  time_t timep = time(0);
+  strftime(time_buf, sizeof(time_buf), "%m-%d-%Y %H:%M:%S ", localtime(&timep));
+  fputs(time_buf, nosqlFS_Data->logFile);
+  vfprintf(nosqlFS_Data->logFile, format, ap);
 }
 /*
 log error and return errno
@@ -48,10 +59,34 @@ void log_retstat(char *func, int retstat){
 }
 
 int log_syscall(char * func, int retstat, int min_ret){
-  log_restat(func, retstat);
+  log_retstat(func, retstat);
   if(retstat < min_ret){
     log_error(func);
     retstat = -errno;
   }
   return retstat;
+}
+
+void log_conn(struct fuse_conn_info * conn){
+  log_msg("fuse_conn_info:\n");
+  log_struct(conn, proto_major, %d, );
+  log_struct(conn, proto_minor, %d, );
+  log_struct(conn, async_read, %d, );
+  log_struct(conn, max_write, %d, );
+  log_struct(conn, max_readahead, %d, );
+  log_struct(conn, capable, %08x, );
+  log_struct(conn, want, %08x, );
+  log_struct(conn, max_background, %d, );
+  log_struct(conn, congestion_threshold, %d, );
+}
+
+void log_fuse_context(struct fuse_context * context){
+  log_msg("fuse_context");
+  log_struct(context, fuse, %08x, );
+  log_struct(context, uid, %d, );
+  log_struct(context, gid, %d, );
+  log_struct(context, pid, %d, );
+  log_struct(context, private_data, %08x, );
+  log_struct(nosqlFS_Data, logFile, %08x, );
+  log_struct(nosqlFS_Data, rootdir, %s, );
 }
--- src/nosqlFS.c
+++ src/nosqlFS.c
@@ -1,12 +1,15 @@
+#include "nosqlFS.h"
+
 #include <fuse.h>
 #include <stdio.h>
 #include <errno.h>
+#include <stdlib.h>
 
 #include "log.h"
 
-static int nosqlFS_getattr(const char * path, struct stat * stbuf){
+int nosqlFS_getattr(const char * path, struct stat * stbuf){
   int result;
-  log_msg("nosqlFS_getattr(char * path = %s, struct stat * stbuf = %08x)", path, stbuf);
+  log_msg("nosqlFS_getattr(char * path = %s, struct stat * stbuf = %08x)\n", path, stbuf);
 
   result = log_syscall("lstat", lstat(path, stbuf), 0);
   if(result == -1){
@@ -15,26 +18,46 @@ static int nosqlFS_getattr(const char * path, struct stat * stbuf){
   return 0;
 }
 
+void *nosqlFS_init(struct fuse_conn_info *conn){
+  log_msg("nosqlFS_init()\n");
+  log_conn(conn);
+  log_fuse_context(fuse_get_context());
+
+  // nosqlFS_Data is a marco for (nosqlFS_state *)private_data
+  return nosqlFS_Data;
+}
+
 struct fuse_operations nosqlFS_oper = {
   .getattr = nosqlFS_getattr,
   .init = nosqlFS_init
-}
+};
 
-int main(int argc, char * argv){
+int main(int argc, char * argv[]){
   int fuse_stat;
-  struct nosqlFS_state * nosqlFS_data;
-  nosqlFS_data = malloc(sizeof(struct nosqlFS_data));
-  if(nosqlFS_data == NULL){
+  struct nosqlFS_state * nosqlFS_init_data;
+  printf("%s\n", "Creating nosqlFS_init_data");
+  nosqlFS_init_data = malloc(sizeof(struct nosqlFS_state));
+  if(nosqlFS_init_data == NULL){
     perror("main calloc");
     abort();
   }
+  printf("%s\n", "Creating finished");
   if(argc < 3){
     perror("wrong parameters");
     abort();
   }
-  nosqlFS_Data->rootdir = realpath(argv[argc - 1], NULL);
-  nosqlFS_Data->logfile = log_open();
-  fuse_stat = fuse_main(argc, argv, &nosqlFS_oper, nosqlFS_Data);
+  printf("%s%s\n", "p1", argv[1]);
+  printf("%s%s\n", "p2", argv[2]);
+  nosqlFS_init_data->rootdir = realpath(argv[argc - 1], NULL);
+  printf("start creating log file\n");
+  nosqlFS_init_data->logFile = log_open();
+  argv[argc - 2] = argv[argc - 1];
+  argv[argc - 1] = NULL;
+  --argc;
+
+  fprintf(stderr, "start fuse_main\n");
+  fuse_stat = fuse_main(argc, argv, &nosqlFS_oper, nosqlFS_init_data);
+  fprintf(stderr, "fuse_main returned %d\n", fuse_stat);
 
   return fuse_stat;
 }
