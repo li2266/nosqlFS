11b2102eb651f02ddf1e32644ae6903ca9276a50
Peng Li <li2266@purdue.edu>
2017-10-24 11:57:11 -0400

    change search to do the experiment pic

--- src/helper/search.py
+++ src/helper/search.py
@@ -10,7 +10,7 @@ def search(key):
 	res = set()
 	for post in collection_invert_list.find({'label' : re.compile(key)}):
 		#rint(post['path'])
-		if post['path'].find("htmldata2500") != -1:
+		if post['path'].find("pic_normal1000") != -1:
 			res.add(post['path'])
 	for path in res:
 		print(path)
